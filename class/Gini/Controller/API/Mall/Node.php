c1cb43171e3453ed3b574a3dd261380b7eeab4f5
Jinlin Li <jinlin.li@geneegroup.com>
2015-07-06 16:39:04 +0800

    'mall hub node 的初始化提交'

---
+++ class/Gini/Controller/API/Mall/Node.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @file Node.php
+* @brief 为mall-vendor/lab-orders/mall-old等app提供API
+* @author Jinlin Li <jinlin.li@geneegroup.com>
+* @version 0.1.0
+* @date 2015-07-06
+ */
+
+namespace Gini\Controller\API\Mall;
+
+/**
+	* @brief 继承自Gini\Controller\API\Base
+ */
+class Node extends \Gini\Controller\API\Base
+{
+
+}
